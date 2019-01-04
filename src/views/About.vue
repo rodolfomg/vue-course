f4705a483180639b5fb46008dd30d0a2a2bc9f7c
Rodolfo Montes <rmontes@concentrico.com.mx>
2019-01-04 16:30:39 -0600

    Interpolations

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ message }}</h1>
+    <h1 v-once>{{ message }}</h1>
+    {{ rawHTML }}
+    <span v-html="rawHTML"></span>
+    <h1>{{ user.name }}</h1>
+
+    {{ user.name.toUpperCase() }}
+
+    {{ user.nme ? user.nme : 'NO tiene nombre' }}
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+
+  data () {
+    return {
+      message: 'This is an about page',
+      rawHTML: '<button>Don\'t click me!</button>',
+      user: {
+        name: 'Luis'
+      },
+      roles: [
+        'admin',
+        'editor',
+        'user'
+      ]
+    }
+  }
+}
+</script>
