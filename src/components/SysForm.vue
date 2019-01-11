f452cac24e1422f83485ba4f88b2b39edd9d0294
Rodolfo Montes <rmontes@concentrico.com.mx>
2019-01-11 15:24:16 -0600

    Adds form example

---
+++ src/components/SysForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="columns is-multiline">
+    <div
+      class="column is-4"
+      v-for="input in inputs"
+      :key="input.id"
+      >
+
+      <div class="field">
+        <label class="label">{{ input.label }}</label>
+        <div class="control">
+          <input
+            v-model="input.value"
+            :type="input.type"
+            :placeholder="input.hint"
+            class="input"
+            >
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'SysForm',
+
+  props: {
+    inputs: { type: Array, required: true }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
