21dc72101c13dfe43ed7bf5252708ce39012ff42
Rodolfo Montes <rmontes@concentrico.com.mx>
2019-01-23 13:52:30 -0600

    Api service connections and environment variables

---
+++ src/services/airtable-config.js
@@ -0,0 +1,6 @@
+const airtableConfig = {
+  url: process.env.VUE_APP_API_URL,
+  token: 'Bearer ' + process.env.VUE_APP_API_TOKEN
+}
+
+export default airtableConfig
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="columns">
+      <div
+        class="column is-4 has-text-centered"
+        v-for="animal in animals"
+        :key="animal.id"
+        >
+        <img :src="animal.fields.Image[0].url" alt="">
+        <h3 class="is-size-4">{{ animal.fields.Name }}</h3>
+        <p>
+          Animal: {{ animal.fields.Animal[0] }}
+        </p>
+        <p>
+          Sex: {{ animal.fields.Sex[0] }}
+        </p>
+        <p>
+          Age: {{ animal.fields.DOB | age }} years old
+        </p>
+        <p>
+          Notes: <br>
+          {{ animal.fields.Notes }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import airtableService from '@/services/airtable-service'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+
+  data () {
+    return {
+      animals: []
+    }
+  },
+
+  created () {
+    airtableService.get('').then(res => {
+      this.animals = res.data.records
+    })
+  },
+
+  filters: {
+    age (dob) {
+      let now = new Date
+      let months = parseInt(dob.split('-')[0] * 12) + parseInt(dob.split('-')[1])
+
+      return ((now.getFullYear() * 12 + now.getMonth() - months) / 12).toFixed(2)
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  img {
+    height: 100px;
+    max-width: 100%;
+    width: auto;
+  }
+</style>
